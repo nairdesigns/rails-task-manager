5cf466ec7cd043c237961cc102b38adefb03ad43
nairdesigns <nairbdg@gmail.com>
2022-02-15 15:07:20 +0900

    href working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,11 +5,10 @@ class TasksController < ApplicationController
   end
 
   def show
-    # @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def new
-    # this empty instance is so the form can build
     @task = Task.new
   end
 
@@ -18,39 +17,32 @@ class TasksController < ApplicationController
     if @task.save
       redirect_to task_path(@task)
     else
-      # if the task doesn save, what should happen?
-      # show the form again, what page was the form on?
       render 'new.html.erb'
     end
   end
 
   def edit
-    # # this instance is so the form can build
-    # @task = Task.find(params[:id])
+    @task = Task.find(params[:id])
   end
 
   def update
-    # @task = Task.find(params[:id])
-    # if @task.update(task_params)
-    #   redirect_to task_path(@task)
-    # else
-    #   # if it doesnt update, show the form again
-    #   render 'edit.html.erb'
-    # end
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task)
+    else
+      render 'edit.html.erb'
+    end
   end
 
   def destroy
-    # # get the instance
-    # @task = Task.find(params[:id])
-    # # destroy the instance
-    # @task.destroy
-    # # redirect somewhere
-    # redirect_to tasks_path
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
 
   def task_params
-    # params.require(:task).permit(:name, :details, :completed)
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
